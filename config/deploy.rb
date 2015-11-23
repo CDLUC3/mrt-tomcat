0faeba3a5bf66e1b23b51ba057a082ff1ec1d06c
mark reyes <mark.reyes@ucop.edu>
2015-11-23 10:37:15 -0800

    more descriptive hint on what to enter

--- config/deploy.rb
+++ config/deploy.rb
@@ -67,7 +67,7 @@ namespace :deploy do
     on roles(:app) do
       execute "cd #{fetch(:deploy_to)}"
       # execute "[ -f #{fetch(:tmp_dir)}/#{fetch(:target)} ] && rm -f #{deploy_to}/temp/#{fetch(:target});"
-      set :build_url, ask('Enter Jenkins artifact URL: ', nil) unless fetch(:build_url)
+      set :build_url, ask('Enter Jenkins artifact URL: ', 'http://builds.cdlib.org/...') unless fetch(:build_url)
       execute "curl --location --silent --output #{fetch(:tmp_dir)}/#{fetch(:target)} #{fetch(:build_url)} || exit 1;"
     end
   end
