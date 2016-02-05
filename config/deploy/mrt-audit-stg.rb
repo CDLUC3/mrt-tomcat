5c7a5cf8d3112f51cc97d15636c99b4c16bad3c6
mark reyes <mark.reyes@ucop.edu>
2016-02-05 15:08:58 -0800

    Audit stage

---
+++ config/deploy/mrt-audit-stg.rb
@@ -0,0 +1,60 @@
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary
+# server in each group is considered to be the first
+# unless any hosts have the primary property set.
+# Don't declare `role :all`, it's a meta role
+#role :app, %w{mark.reyes@ucop.edu}
+#role :web, %w{mark.reyes@ucop.edu}
+
+# Extended Server Syntax
+# ======================
+# This can be used to drop a more detailed server
+# definition into the server list. The second argument
+# something that quacks like a hash can be used to set
+# extended properties on the server.
+# ---- Needed for non-rails deployment???
+set :rails_env, "mrt-audit-stg"
+
+puts "----- mrt-audit-stg branch of https://hg.cdlib.org/tomcat8_catalina_base -----"
+set :repo_url, "https://hg.cdlib.org/tomcat8_catalina_base"
+set :branch, "mrt-audit-stg"
+
+set :application, "merritt-audit"
+set :build_url, "http://builds.cdlib.org/view/Merritt/job/mrt-audit-default/ws/audit-war/vm-stg/mrtaudit.vm-stg.war"
+
+set :target, "mrtausit.war"
+set :deploy_to, "/dpr2/apps/audit37001"
+
+set :tomcat_pid, "#{fetch(:deploy_to)}/audit.pid"
+set :tomcat_log, "#{fetch(:deploy_to)}/shared/log/tomcat.log"
+
+server "uc3-mrtaudit-stg", user: "dpr2", roles: %w{web app}
+
+namespace :custom do
+  desc 'Custom deploy action`'
+  task :deploy_bits do
+    on roles(:app) do
+        puts "No custom deploy_bits action"
+    end
+  end
+
+  desc 'Custom pre-stop action'
+  task :prestop do
+    on roles(:app) do
+        puts "Shutdown Audit Application"
+        execute "/usr/bin/curl --silent -X POST http://localhost:37001/mrtaudit/service/stop?t=xml"
+        execute "sleep 5"
+    end
+  end
+
+  desc 'Custom post-start action'
+  task :poststart do
+    on roles(:app) do
+        puts "Startup Audit Application"
+        execute "sleep 5"
+        execute "/usr/bin/curl --silent -X POST http://localhost:37001/mrtaudit/service/start?t=xml"
+    end
+  end
+
+end
