0f2938f749be10f66334b2a05959096f7c34f9c5
mark reyes <mark.reyes@ucop.edu>
2016-02-10 15:39:26 -0800

    change hostname

--- config/deploy/mrt-node-stg.rb
+++ config/deploy/mrt-node-stg.rb
@@ -29,7 +29,7 @@ set :deploy_to, "/dpr2/apps/node35801"
 set :tomcat_pid, "#{fetch(:deploy_to)}/node.pid"
 set :tomcat_log, "#{fetch(:deploy_to)}/shared/log/tomcat.log"
 
-server "replic01-aws-stg.cdlib.org", user: "dpr2", roles: %w{web app}
+server "replic-aws-stg.cdlib.org", user: "dpr2", roles: %w{web app}
 
 # custom
 namespace :custom do
