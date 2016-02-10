5a36c37b442505f57289f7bc33ca9165c7348fa0
mark reyes <mark.reyes@ucop.edu>
2016-02-10 11:12:02 -0800

    rename of host

--- config/deploy/mrt-replic-stg.rb
+++ config/deploy/mrt-replic-stg.rb
@@ -32,7 +32,7 @@ set :tomcat_log, "#{fetch(:deploy_to)}/shared/log/tomcat.log"
 # additional directories needed by storage
 set :linked_dirs, fetch(:linked_dirs).push("curl")
 
-server "replic01-aws-stg.cdlib.org", user: "dpr2", roles: %w{web app}
+server "replic-aws-stg.cdlib.org", user: "dpr2", roles: %w{web app}
 
 # custom
 namespace :custom do
