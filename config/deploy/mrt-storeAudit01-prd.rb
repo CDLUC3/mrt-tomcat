0cce6c274e2a7240bb7abfbc4bbddf2fb0342ca9
mark reyes <mark.reyes@ucop.edu>
2016-03-15 16:06:35 -0700

    Add storage service that are used by Audit

---
+++ config/deploy/mrt-storeAudit01-prd.rb
@@ -0,0 +1,56 @@
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary
+# server in each group is considered to be the first
+# unless any hosts have the primary property set.
+# Don't declare `role :all`, it's a meta role
+#role :app, %w{mark.reyes@ucop.edu}
+#role :web, %w{mark.reyes@ucop.edu}
+
+# Extended Server Syntax
+# ======================
+# This can be used to drop a more detailed server
+# definition into the server list. The second argument
+# something that quacks like a hash can be used to set
+# extended properties on the server.
+# ---- Needed for non-rails deployment???
+set :rails_env, "mrt-storeAudit01-prd"
+
+puts "----- mrt-storeAudit01-prd branch of https://hg.cdlib.org/tomcat8_catalina_base -----"
+set :repo_url, "https://hg.cdlib.org/tomcat8_catalina_base"
+set :branch, "mrt-storeAudit01-prd"
+
+set :application, "merritt-storeAudit"
+set :build_url, "http://builds.cdlib.org/view/Merritt/job/mrt-storeCloud%20(default)/ws/store-war/vm-read/storage.vm-read.war"
+
+set :target, "storage.war"
+set :deploy_to, "/dpr2/apps/storage35122"
+
+set :tomcat_pid, "#{fetch(:deploy_to)}/storage.pid"
+set :tomcat_log, "#{fetch(:deploy_to)}/shared/log/tomcat.log"
+
+server "audit01-aws.cdlib.org", user: "dpr2", roles: %w{web app}
+
+namespace :custom do
+
+  desc 'Custom deploy action'
+  task :deploy_bits do
+    on roles(:app) do
+        puts "No custom deploy_bits actions"
+    end
+  end
+
+  desc 'Custom pre-stop action'
+  task :prestop do
+    on roles(:app) do
+        puts "No custom prestop action"
+    end
+  end
+
+  desc 'Custom post-start action'
+  task :poststart do
+    on roles(:app) do
+        puts "No custom poststart action"
+    end
+  end
+end
