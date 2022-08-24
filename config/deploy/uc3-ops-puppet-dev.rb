4374eb4a71b9d4c92b06f5328cc5158c4bd8b143
Ashley Gould <agould@ucop.edu>
2022-08-23 17:39:11 -0700

    same

--- config/deploy/uc3-ops-puppet-dev.rb
+++ config/deploy/uc3-ops-puppet-dev.rb
@@ -6,10 +6,10 @@ set :user,             ENV['USER']                     || 'dpr2'
 set :group,            ENV['GROUP']                    || 'dpr2'
 set :home_dir,         ENV['HOME']                     || '/apps/dpr2'
 set :service,          ENV['MERRITT_SERVICE']          || 'audit'
-set :semantic_version, ENV['MERRITT_SERVICE_RELEASE'], || 'undefined'
-set :artifact_server,  ENV['ARTIFACT_SERVER'],         || 'http://builds.cdlib.org'
-set :artifact_path,    ENV['ARTIFACT_PATH'],           || '/userContent/mrt-audit'
-set :artifact_name,    ENV['ARTIFACT_NAME'],           || "mrt-audit-#{fetch(:semantic_version)}.war"
+set :semantic_version, ENV['MERRITT_SERVICE_RELEASE']  || 'undefined'
+set :artifact_server,  ENV['ARTIFACT_SERVER']          || 'http://builds.cdlib.org'
+set :artifact_path,    ENV['ARTIFACT_PATH']            || '/userContent/mrt-audit'
+set :artifact_name,    ENV['ARTIFACT_NAME']            || "mrt-audit-#{fetch(:semantic_version)}.war"
 
 set :target, "#{fetch(:artifact_name)}"
 set :build_url, "#{fetch(:artifact_server)}#{fetch(:artifact_path)}/#{fetch(:artifact_name)}"
